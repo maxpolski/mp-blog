d9ccaa6168a75f0becf53b0ac3fa3f9968e06d73
Max <maxpolski.job@yandex.ua>
2016-02-11 16:13:24 +0200

    webpack configured + static folder defined

--- app.js
+++ app.js
@@ -4,11 +4,13 @@ import router  from './routes';
 const app = express();
 const routes = new router();
 
+app.use(express.static('./public'));
+
 app.get('/', (req, res) => routes.index(req, res));
 
 
 export const run = module.exports.run = function () {
-  app.listen(8080, `localhost`, function (req, res) {
+  app.listen(8080, `localhost`, (req, res) => {
     console.log('listening on port 8080');
   });
 }
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,30 @@
+import fs from 'fs';
+
 class routes {
   index(req, res) {
-    res.status(200).end('ok');
+    let pathToView = './public/views/index.html';
+    let response = new Promise((resolve, reject) => {
+      fs.access(pathToView, fs.R_OK, (err) => {
+        if(err) {
+          reject(err);
+        }
+        resolve();
+      });
+    });
+
+    response
+      .then(() => {
+        return new Promise ((resolve, reject) => {
+          fs.readFile(pathToView, (err, data) => {
+            if(err) {
+              reject(err);
+            }
+            resolve(data);
+          })
+        })
+      })
+      .then( data => res.set('Content-Type', 'text/html').send(data).status(200))
+      .catch( err => res.status(500).end('not ok'));
   }
 }
 
