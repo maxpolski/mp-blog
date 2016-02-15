b49f98935dfbe05ce3e2c688984ce59831f8f3c7
Max Polski <maxpolski.job@yandex.ua>
2016-02-15 08:22:59 +0200

    frontend started

--- public/js/src/components/Layout.jsx
+++ public/js/src/components/Layout.jsx
@@ -8,13 +8,19 @@ export default class Layout extends Component {
                  <span>MP</span>
                </div>
                <ul type="none" className="menu-elements">
-                 <li>Blog</li>
-                 <li>About me</li>
-                 <li>Projects</li>
-                 <li>Sand Box</li>
+                 <li className="color-transition"><span>Blog</span></li>
+                 <li className="color-transition"><span>Projects</span></li>
+                 <li className="color-transition"><span>Sand Box</span></li>
+                 <li className="color-transition"><span>About me</span></li>
+                 <li className="animated-cross">
+                   <i></i>
+                   <i></i>
+                 </li>
+                 <div className="float-fixer"></div>
                </ul>
+               <div className="float-fixer"></div>
              </nav>
-             <h1>Hello World</h1>
+             <h1>Пример кириллицы</h1>
            </div>
   }
 }
